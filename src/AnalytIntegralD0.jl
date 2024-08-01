5a67cd7ea4543c5242728efb35a62b6ef8c8b0e7
PeilinZhang <pl127116@gmail.com>
2024-08-01 11:57:25 +0200

    complete

--- src/AnalytIntegralD0.jl
+++ src/AnalytIntegralD0.jl
@@ -7,7 +7,7 @@ using CompScienceMeshes
 #defeine globla constants and variables
 const zerotol = 3e-16 #2e-16 gives error in determining small h4 = 2.482534153247273e-16 as non zero when it seems to be zero
 const testMode::Int = 0 #change this into 1 will enable many print parts within the code.
-case = 0
+global case = 0
 
 include("GalerkinLaplaceTriGS.jl")
 include("GSorthogonalization.jl")
